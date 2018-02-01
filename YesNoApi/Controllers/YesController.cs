fd43ff54eacaa9f0adbdb854e613f5c8ed2cd6a4
gregoire <gregoire.ternon@orone.fr>
2018-02-01 15:52:47 +0100

    first documented version

---
+++ YesNoApi/Controllers/YesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace YesNoApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Yes")]
+    public class YesController : Controller
+    {
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            HttpClient client = new HttpClient();
+            string res = await client.GetStringAsync("https://yesno.wtf/api/");
+            return JsonConvert.DeserializeObject<object>(res);
+            
+        }
+    }
+}
--- YesNoApi/Startup.cs
+++ YesNoApi/Startup.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace YesNoApi
 {
@@ -23,16 +27,30 @@ namespace YesNoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(a=>
+            {
+                a.SwaggerDoc("v1", new Info {Title="SayYes" });
+            });
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+
+            app.UseDefaultFiles().UseStaticFiles(new StaticFileOptions()
             {
-                app.UseDeveloperExceptionPage();
-            }
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot")),
+                RequestPath = new PathString("/static")
+            });
+
+
+            app.UseSwagger();
+            app.UseSwaggerUI(a=>
+            {
+                a.SwaggerEndpoint("/swagger/v1/swagger.json", "SayYes Documentation");
+            });
+
 
             app.UseMvc();
         }
