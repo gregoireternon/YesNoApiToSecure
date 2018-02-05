bcfe95472db875f4107ee34983d8b2a43d25a7b6
-g <gregoire.ternon@orone.fr>
2018-02-05 15:00:22 +0100

    Adding No Controller

--- YesNoApi/Controllers/NoController.cs
+++ YesNoApi/Controllers/NoController.cs
@@ -10,16 +10,16 @@ using Newtonsoft.Json;
 namespace YesNoApi.Controllers
 {
     [Produces("application/json")]
-    [Route("api/Yes")]
-    public class YesController : Controller
+    [Route("api/No")]
+    public class NoController : Controller
     {
         [HttpGet]
         public async Task<object> Get()
         {
             HttpClient client = new HttpClient();
-            string res = await client.GetStringAsync("https://yesno.wtf/api?force=yes");
+            string res = await client.GetStringAsync("https://yesno.wtf/api?force=no");
             return JsonConvert.DeserializeObject<object>(res);
-            
+
         }
     }
 }
